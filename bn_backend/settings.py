98ce880ff58e150386548b4955d244ea0db684c2
Samuel Ajayi <sirmisell@gmail.com>
2026-04-21 23:50:45 +0100

    all0w h9st

--- bn_backend/settings.py
+++ bn_backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [host.strip() for host in os.getenv('ALLOWED_HOSTS', '*').split(',') if host.strip()]
 
 
 # Application definition
